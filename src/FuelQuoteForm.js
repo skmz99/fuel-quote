7ff1359cb44024141b107f8119fa35622062054d
skmz99 <83142392+skmz99@users.noreply.github.com>
2022-06-24 16:32:18 -0500

    practice

---
+++ src/FuelQuoteForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './index.css';
+
+function FuelQuoteForm(){
+    return(
+        <div class="middle">
+            <h1>This is Fuel Quote Form</h1>
+            <h2>Idk what im doing</h2>
+        </div>
+    );
+}
+
+export default FuelQuoteForm;
---
+++ src/FuelQuoteHistory.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './index.css';
+
+function FuelQuoteHistory(){
+    return(
+        <div class="middle">
+            <h1>This is Fuel Quote History</h1>
+        </div>
+    );
+}
+
+export default FuelQuoteHistory;
